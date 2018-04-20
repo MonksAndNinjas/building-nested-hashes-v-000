1313a62fef3ec745ff752c7345ba574ef46a0ec9
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-20 20:36:10 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,9 +15,16 @@ def third_challenge
         :status => "alive"
         },
       :hero_friends => [
-        :name => "Benvolio",
-        :age => "17",
-        :attitude => "worried"
+        {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+        },
+        {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+        }
       ]
    },
    :capulet => {
@@ -34,7 +41,9 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        
+      ]
    }
   }
 
